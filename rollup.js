5526f2525294f3e94bcf3949d319b5d559f3a10a
amekusa <hello@amekusa.com>
2024-02-16 01:48:28 +0900

    add rollup.js

---
+++ rollup.js
@@ -0,0 +1,39 @@
+/**
+ * Rollup Config
+ *
+ * Prerequisites:
+ *   npm i --save-dev rollup @rollup/plugin-node-resolve
+ *
+ * Use with:
+ *   rollup -c rollup.js
+ */
+
+import rNode from '@rollup/plugin-node-resolve';
+
+const output = {
+	name: '',
+	sourcemap: true,
+	indent: false,
+};
+
+export default {
+	input: 'src/main.js',
+	output: [
+		{
+			file: 'dist/import/bundle.mjs',
+			format: 'es',
+			...output
+		},
+		{
+			file: 'dist/require/bundle.cjs',
+			format: 'cjs',
+			...output
+		},
+	],
+	treeshake: true,
+	plugins: [
+		rNode({ // support importing npm packages
+			browser: false,
+		}),
+	],
+};
